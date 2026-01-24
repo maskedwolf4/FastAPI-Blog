f5ec3b22e0804dbce5c18cb3b2db64fe25b076b5
maskedwolf4 <meetwadekar7@gmail.com>
2026-01-24 21:35:46 +0530

    Adding models for db

--- database.py
+++ database.py
@@ -5,9 +5,9 @@ from sqlalchemy.orm import DeclarativeBase, sessionmaker
 SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, 
-                       connect_args={"check_same thread":False})
+                       connect_args={"check_same thread":False}) # connection to db
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # session -> transaction to db
 
 class Base(DeclarativeBase):
     pass
---
+++ models.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from datetime import UTC,datetime
+
+from sqlalchemy import DateTime, ForeignKey, Integer, String, Text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id:Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        nullable=False,
+        index=True,
+    )
+    date_posted: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(UTC),
+    )
+
+    author: Mapped[User] = relationship(back_populates="posts")
