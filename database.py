b54d021ae8cce3d60147f377887f836cacc297f6
maskedwolf4 <meetwadekar7@gmail.com>
2026-02-03 04:34:16 +0530

    Added async working/functionality

--- database.py
+++ database.py
@@ -1,17 +1,24 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
 
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./blog.db"
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./blog.db"
+engine = create_async_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, 
-                       connect_args={"check_same_thread":False}) # connection to db
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # session -> transaction to db
 
 class Base(DeclarativeBase):
     pass
 
-def get_db():
-    with SessionLocal() as db:
-        yield db
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
